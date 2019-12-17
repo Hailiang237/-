0a42e691f2d3033e3ac8b43517f083acaaaa1d52
zhuhailiang <524577590@qq.com>
2019-12-17 18:01:24 +0800

    完成banner,开始写头部下面的导航栏效果

--- src/script/index.js
+++ src/script/index.js
@@ -1,4 +1,4 @@
 import{tool,$} from "./tool.js";//引入模块
-import{Banner} from "./banner.js"
-new Banner().init()
-console.log($(".banner-num")) 
+import{Banner,Head_nav} from "./effect.js";//主页的js效果
+new Banner().init();
+new Head_nav().init();
