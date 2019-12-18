2f5f8aef9a19945cf30c1fc15aca424c9d86524c
zhuhailiang <524577590@qq.com>
2019-12-18 09:50:32 +0800

    完成头部导航栏hover效果的js实现

--- src/script/effect.js
+++ src/script/effect.js
@@ -45,81 +45,120 @@ class Banner {//轮播图
         this.pindex = this.nindex;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 导航栏hover效果
 class Head_nav {
     constructor() {
         this.head_nav_sub = $(".head-nav-sub");//二级导航栏的盒子, 0-5
         this.head_nav_ul_li = $(".head-nav-ul li");//一级导航栏的盒子,1-6
         this.head_nav_ul = $(".head-nav-ul");
         this.flag = false;//是否已经打开了二级导航栏,true:切换li时 直接显示,
-        this.index = 0;
+        this.index =1;
+        this.pindex = 1;
+        this.timer1= null;//moveout 显示的定时器
+        this.timer2= null;//hide 隐藏的定时器
+        this.timer3 = null;//延时定时器
 
         //移动到li下面 二级导航动画显示, 
     }
     init() {
-        this.set();
-    }
-    set() {
         let _this = this;
         for (let i = 1; i < this.head_nav_ul_li.length; i++) {
-            this.head_nav_ul_li[i].onmouseover = function () {
+            this.head_nav_ul_li[i].onmouseenter = function () {//onmouseenter 当用户将鼠标指针移动到对象内时触发。
                 _this.index = i;
-                if (_this.flag ===false) {
-                    console.log("move:"+i)
-                    
-                    _this.moveout();
-                } else {
-                    
-                    _this.change();
-                }
+                //延时执行  
+                clearTimeout(this.timer3);
+                clearTimeout(this.timer2);
+                clearTimeout(this.timer1);
+                
+                // console.log("已经清楚定时器timer3,2,1")
+                _this.timer3 = setTimeout(function(){
+                    if (_this.flag === false) {
+                        _this.moveout();
+                    } else {
+                        _this.change();
+                    }
+                },400)
+ 
             }
         }
-        this.head_nav_ul.onclick = function(){
-            console.log(_this.head_nav_ul)
+        this.head_nav_ul.onmouseleave = function () {//onmouseleave 当用户将鼠标指针移出对象边界时触发。            
             _this.hide()
+        }
+        this.head_nav_ul_li[0].onmouseenter = function () {
+            _this.hide();
         }
     }
 
-    hide(){
-        console.log("hide:"+this.index);
+    hide() {
+        // console.log("hide:" + this.index,this.pindex,this.flag);
+        clearTimeout(this.timer3);
+        // console.log("已经清楚定时器timer3","out")
         this.flag = false;
         let _this = this;
         let speed = 5;
-        let timer2 = setInterval(function () {
-            _this.head_nav_sub[_this.index - 1].style.height = _this.head_nav_sub[_this.index - 1].offsetHeight - speed + "px";
-            speed++;
-            if (_this.head_nav_sub[_this.index - 1].offsetHeight < 0) {
-                _this.head_nav_sub[_this.index - 1].style.height = "0px";
-                clearInterval(timer2);    
-                _this.head_nav_sub[_this.index - 1].style.display = "none";
-            }
-        }, 20);
+       
+        this.timer3 = setTimeout(function(){ 
+            clearInterval(_this.timer2);
+            _this.timer2 = setInterval(function () {
+                _this.head_nav_sub[_this.index - 1].style.height = _this.head_nav_sub[_this.index - 1].offsetHeight - speed + "px";
+                speed++;
+                if (_this.head_nav_sub[_this.index - 1].offsetHeight <= 10) {
+                    _this.head_nav_sub[_this.index - 1].style.height = "0px";
+                    _this.head_nav_sub[_this.index - 1].style.display = "none";
+                    clearInterval(_this.timer2);
+                    // console.log("已清除定时器timer2")
+    
+    
+                }
+            }, 8);
+        },200)
+        
+        
     }
     change() {
-        console.log("change:"+this.index);
-        this.flag=true;//在里面
-        for (let j = 0; j < this.head_nav_sub.length; j++) {
-            this.head_nav_sub[j].style.display = "none";
-            this.head_nav_sub[j].style.height = "0px";
-        }
+        // console.log("change:" + this.index,this.pindex,this.flag);
+        this.flag = true;//在里面
+        // clearInterval(this.timer);
+        // clearInterval(this.timer2);
+        this.head_nav_sub[this.pindex - 1].style.display = "none";
+        this.head_nav_sub[this.pindex - 1].style.height = "0px";
+
         this.head_nav_sub[this.index - 1].style.height = "230px";
         this.head_nav_sub[this.index - 1].style.display = "block";
-       
+
+        this.pindex = this.index;
     }
     moveout() {
-        console.log("moveout:"+this.index);
+        // console.log("moveout:" + this.index,this.pindex,this.flag);
         let _this = this;
         let speed = 5;
         this.head_nav_sub[_this.index - 1].style.display = "block";
-        let timer = setInterval(function () {
+        clearInterval(this.timer1);
+        this.timer1 = setInterval(function () {
             _this.flag = true;//在
             _this.head_nav_sub[_this.index - 1].style.height = _this.head_nav_sub[_this.index - 1].offsetHeight + speed + "px";
             speed++;
-            if (_this.head_nav_sub[_this.index - 1].offsetHeight >= 230) {
+            if (_this.head_nav_sub[_this.index - 1].offsetHeight >= 220) {
                 _this.head_nav_sub[_this.index - 1].style.height = "230px";
-                clearInterval(timer);
-                
+                clearInterval(_this.timer1);
+                // console.log("已清除定时器timer1")
+                _this.pindex = _this.index;
             }
-        }, 20)
+        }, 8)
     }
 }
 
